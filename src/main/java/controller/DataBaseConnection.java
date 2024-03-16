7d8ac4dce8b14954d137b26302438d6212e2a535
Vinícius Gabriel Pereira Leitão <viniciusggabriel989@protonmail.com>
2024-03-16 20:13:15 -0300

    feat: Adiciona a função de select e close do banco de dados banco de dados
    - Método de select ao banco de dados, o método se consistem em receber a query de select e o valor da coluna que será buscada do select e por meio de tentativa retorna um array com os valores
    
    - Também adicionara um método de fechamento do banco de dados,

--- src/main/java/controller/DataBaseConnection.java
+++ src/main/java/controller/DataBaseConnection.java
@@ -1,8 +1,10 @@
 package controller;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import java.sql.Connection;
-import java.sql.DriverManager;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataBaseConnection {
     Dotenv dotenv = Dotenv.load(); // Carrega as variáveis do .env
@@ -10,15 +12,54 @@ public class DataBaseConnection {
     private final String user = dotenv.get("DB_USER");
     private final String password = dotenv.get("DB_PASSWORD");
 
-    // Método de conexão ao DB
-    public Connection dataBaseConnection() {
+    // Método de conexão ao Banco de dados
+    public static Connection dataBaseConnection(String url, String user, String password) {
         try {
-            String driver = "com.mysql.cj.jdbc.Driver";
-            Class.forName(driver);
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Passa o driver para conexão
             System.out.println("🔥 Conexão ao banco de dados bem sucedida");
             return DriverManager.getConnection(url, user, password);
         } catch (Exception error) {
             System.out.println("Ouve um erro ao se conectar com o MySql:" + error);
+            return null;
+        }
+    }
+
+    public static void dataBaseCloseConnection(Connection connection) {
+        try {
+            if (connection != null) connection.close();
+            System.out.println("❌ Conexão encerrada com o banco de dados");
+        } catch (Exception error) {
+            // SQLException exception
+            System.out.println("Erro ao encerrar conexão com o banco de dados, ou já foi encerrada: " + error);
+        }
+    }
+
+    // Método de select, com where dentro do banco de dados
+    public Object[] querySelectDataBase(String query, String valueColumn) {
+        List<Object> resultList = new ArrayList<>(); // Recebe qualquer tipo de valor
+
+        // Tenta a conexão com banco de dados
+        try (Connection connection = dataBaseConnection(url, user, password)) {
+            assert connection != null; // Caso diferente de null passa se for null retorna erro
+
+            // Tenta criar o cursor dentro do banco de dados e executar a query
+            try (PreparedStatement statement = connection.prepareStatement(query);
+                 ResultSet resultSet = statement.executeQuery()) {
+
+                while (resultSet.next()) {
+                    String valuesResult = resultSet.getString(valueColumn);
+                    resultList.add(valuesResult); // Adiciona dentro da lista de objetos o resultado
+                }
+
+                return resultList.toArray();
+            } catch (Exception error) {
+                System.out.println("Erro ao preparar cursor no banco de dados: " + error);
+                return null;
+            } finally {
+                dataBaseCloseConnection(connection);
+            }
+        } catch (Exception error) {
+            System.out.println("Ouve um erro ao se conectar com o banco de dados, verifique os parâmetros passados: " + error);
             return null;
         }
     }
