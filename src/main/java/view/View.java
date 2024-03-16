7092243255e45c33f22ce6340af861915e613ada
Vinícius Gabriel Pereira Leitão <viniciusggabriel989@protonmail.com>
2024-03-16 20:13:39 -0300

    feat: Adiciona um novo botão e usa o metodo de select do banco de dados

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -8,27 +8,46 @@ import java.awt.event.ActionListener;
 
 public class View {
     // Atributos
-    private JButton buttonMsg;
+    private JButton buttonSelect;
     private JPanel panelMain;
+    private JButton buttonInsert;
 
     // Método main
     public static void main(String[] args) {
+        setView();
+    }
+
+    // Método que setá os atributos do frame
+    public static void setView() {
         JFrame frame = new JFrame("View"); // Titulo do frame
         frame.setContentPane(new View().panelMain); // Coloca tudo que está dentro do panelMain para dentro do frame
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-        frame.setSize(400, 400);
+        frame.setSize(600, 400);
     }
 
-    // Função view
+    // Função view, java swing insere para adicionar os eventos do projeto
     private View() {
-        buttonMsg.addActionListener(new ActionListener() {
+        buttonSelect.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 DataBaseConnection dataBase = new DataBaseConnection();
-                String result = String.valueOf(dataBase.dataBaseConnection());
-                JOptionPane.showMessageDialog(null, result);
+                Object[] resultSelectPass = dataBase.querySelectDataBase("SELECT * FROM PANEL_PASS", "PASS");
+                Object[] resultSelectPassType = dataBase.querySelectDataBase("SELECT * FROM PANEL_PASS", "PASS_TYPE");
+
+                if (resultSelectPass != null && resultSelectPassType != null) {
+
+                    // Loop que define os valores dentro do array como object
+                    for (Object resultPass : resultSelectPass) {
+                        for (Object resultPassType : resultSelectPassType) {
+                            System.out.println(String.valueOf(resultPassType + " " + resultPass));
+                        }
+                    }
+
+                } else {
+                    System.out.println("Nenhum resultado encontrado.");
+                }
             }
         });
     }
